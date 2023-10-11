2bed43bcecf45d47d4cafdc00585d10e3b02f839
Leena <bleenaweb238@gmail.com>
2023-10-11 19:38:37 +0530

    Improvement in #1 solution

--- solution-1.rb
+++ solution-1.rb
@@ -8,3 +8,33 @@ class User < ApplicationRecord
 end
 
 # Updated the method that returns the cache count to use memoization
+
+# Here I am proposing #1 problem solution using Redis.
+class User < ApplicationRecord
+ has_many :hits, after_add: :increment_hits
+
+ def count_hits
+ $redis.get(hit_key) || get_set_hits
+ end
+
+ private
+
+ def get_set_hits
+ api_hits = hits.where('created_at > ?', Time.now.beginning_of_month).count
+ $redis.set(hit_key, api_hits)
+ api_hits
+ end
+
+ def increment_hits
+ $redis.get(hit_key) ? $redis.incr(hit_key) : get_set_hits
+ end
+
+ def hit_key
+ "hits_count_#{id}"
+ end
+end
+
+# Suppose we are getting redis object inside global variable ($redis)
+# We can call the increment hits method using a callback
+
+
